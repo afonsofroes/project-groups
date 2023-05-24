f598baf3a4ed2276c9ec8bc6c2c3beff913ad57d
Afonso Froes <afonsofroes@gmail.com>
2023-05-24 09:53:06 +0100

    tidied up variable names

--- joao_model.py
+++ joao_model.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy.optimize import linprog
 import pandas as pd
 
+
 def make_groups(data_df):
     # x_c is the number of extra columns for any necessary processing
     x_c = 1
@@ -49,7 +50,7 @@ def make_groups(data_df):
     temp = np.zeros([n_projects, col])
     for i in range(n_projects):
         for j in range(n_students):
-            temp[i, j*n_projects + i] = -1
+            temp[i, j * n_projects + i] = -1
         temp[i, col - x_c - n_projects + i] = 4
     A_ub = np.concatenate([A_ub, temp])
     b_ub = np.concatenate([b_ub, [1] * n_projects])
@@ -57,7 +58,7 @@ def make_groups(data_df):
     temp = np.zeros([n_projects, col])
     for i in range(n_projects):
         for j in range(n_students):
-            temp[i, j*n_projects + i] = 1
+            temp[i, j * n_projects + i] = 1
         temp[i, col - x_c - n_projects + i] = -4
     A_ub = np.concatenate([A_ub, temp])
     b_ub = np.concatenate([b_ub, [0] * n_projects])
@@ -67,15 +68,12 @@ def make_groups(data_df):
     for i, proj in data_df[["pitched"]].itertuples():
         if proj.is_integer():
             pitch_dict[i] = int(proj)
-    print(pitch_dict)
     temp = np.zeros([len(pitch_dict), col])
     for i, (student_i, proj) in enumerate(pitch_dict.items()):
         temp[i, student_i * n_projects + proj] = 1
-        temp[i, col - 2*x_c] = -1
-    print(temp)
+        temp[i, col - 2 * x_c] = -1
     A = np.concatenate([A, temp])
     b = np.concatenate([b, [0] * len(pitch_dict)])
-
 
     # minimax
     temp = np.zeros([n_students * n_projects, col])
@@ -90,53 +88,54 @@ def make_groups(data_df):
     c = np.zeros(col)
     c[col - x_c] = 1
 
-
-    res = linprog(c, A_eq=A, b_eq=b, A_ub=A_ub, b_ub=b_ub, integrality=1)
+    result = linprog(c, A_eq=A, b_eq=b, A_ub=A_ub, b_ub=b_ub, integrality=1)
 
     print(
         "Optimal Value: ",
-        round(res.fun, ndigits=2),
+        round(result.fun, ndigits=2),
         "\nx Values: ",
-        res.x,
+        result.x,
         "\nNumber of iterations performed: ",
-        res.nit,
+        result.nit,
         "\nStatus: ",
-        res.message,
+        result.message,
     )
 
-    # setting best result as constraint
+    # setting best resultult as constraint
     temp = np.zeros([1, col])
     temp[0, col - x_c] = 1
     A_ub = np.concatenate([A_ub, temp])
-    b_ub = np.concatenate([b_ub, [res.x[-1]]])
+    b_ub = np.concatenate([b_ub, [result.x[-1]]])
 
     # minimizing total pref
     c = np.concatenate([data, [0] * (n_projects + 1)])
-    res = linprog(c, A_eq=A, b_eq=b, A_ub=A_ub, b_ub=b_ub, integrality=1)
+    result = linprog(c, A_eq=A, b_eq=b, A_ub=A_ub, b_ub=b_ub, integrality=1)
 
     print(
         "Optimal value:",
-        round(res.fun, ndigits=2),
+        round(result.fun, ndigits=2),
         "\nx values:",
-        res.x,
+        result.x,
         "\nNumber of iterations performed:",
-        res.nit,
+        result.nit,
         "\nStatus:",
-        res.message,
+        result.message,
     )
 
-    print("Results: ")
+    print("Resultults: ")
     print(
-        (res.x[: -n_projects - x_c] * data)
+        (result.x[: -n_projects - x_c] * data)
         .reshape([n_students, n_projects])
         .astype(int)
     )
 
     print("\nChosen Projects: ")
-    print(res.x[-n_projects - x_c : -x_c])
+    print(result.x[-n_projects - x_c : -x_c])
 
-    res_df = pd.DataFrame(
-        (res.x[: -n_projects - x_c] * data).reshape([n_students, n_projects]).astype(int)
+    result_df = pd.DataFrame(
+        (result.x[: -n_projects - x_c] * data)
+        .reshape([n_students, n_projects])
+        .astype(int)
     )
 
-    return res_df
+    return result_df
