7ba2f7696fca6d538fa03623e185c761addf09b6
Afonso Froes <afonsofroes@gmail.com>
2023-06-01 10:10:40 +0100

    replaced 1 with x_c

--- model.py
+++ model.py
@@ -72,7 +72,7 @@ def make_groups(data_df):
     temp = np.zeros([len(pitch_dict), col])
     for i, (student_i, proj) in enumerate(pitch_dict.items()):
         temp[i, student_i * n_projects + proj] = 1
-        temp[i, -n_projects + proj - 1] = -1
+        temp[i, -n_projects + proj - x_c] = -1
     print(temp)
     print(pitch_dict)
     A = np.concatenate([A, temp])
